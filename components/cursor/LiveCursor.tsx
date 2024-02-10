b780b7f4ec2ad0b58b7b7018628c43f20d0e6cd4
unknown <kumar.ankit.101012@gmail.com>
2024-02-10 21:01:30 +0530

    modified

--- components/cursor/LiveCursor.tsx
+++ components/cursor/LiveCursor.tsx
@@ -3,18 +3,16 @@ import Cursor from "./Cursor";
 import { COLORS } from "@/constants";
 
 const LiveCursor = ({ others }: LiveCursorProps) => {
-  return others.map(({connectionId, presence}) => {
-    if (!presence?.cursor) return null;
+  return others.map(({ connectionId, presence }) => {
+    if (presence==null || !presence?.cursor) return null;
     return (
-      <div>
-        <Cursor
-          key={connectionId}
-          color={COLORS[Number(connectionId) % COLORS.length]}
-          x={presence.cursor.x}
-          y={presence.cursor.y}
-          message={presence.message}
-        />
-      </div>
+      <Cursor
+        key={connectionId}
+        color={COLORS[Number(connectionId) % COLORS.length]}
+        x={presence.cursor.x}
+        y={presence.cursor.y}
+        message={presence.message}
+      />
     );
   });
 };
